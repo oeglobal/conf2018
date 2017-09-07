c9a216c3bbb2d1229fc8e78b484644e1d8a33ac6
Jure Cuhalev <jure@cuhalev.com>
2017-09-07 23:06:14 +0200

    Move euro sign to the left

--- inc/filters.php
+++ inc/filters.php
@@ -1,1 +1,17 @@
 <?php
+
+add_filter( 'gform_currencies', 'gw_modify_currencies' );
+function gw_modify_currencies( $currencies ) {
+
+	$currencies['EUR'] = array(
+		'name'               => esc_html__( 'Euro', 'gravityforms' ),
+		'symbol_left'        => '&#8364;',
+		'symbol_right'       => '',
+		'symbol_padding'     => ' ',
+		'thousand_separator' => '.',
+		'decimal_separator'  => ',',
+		'decimals'           => 2
+	);
+
+	return $currencies;
+}
