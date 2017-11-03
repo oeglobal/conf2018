9a0d2fbabb3edf82df4a49bad9a5f7c6686fc2cd
Jure Cuhalev <jure@cuhalev.com>
2017-11-03 12:17:59 +0100

    Link blog posts

--- partials/content-post.php
+++ partials/content-post.php
@@ -2,7 +2,7 @@
 
     <div class="row">
         <div class="medium-offset-1 medium-10 columns">
-            <h1 class="post--title"><?php the_title(); ?></h1>
+            <h1 class="post--title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
             <h2 class="post--author">Contributed by <?= get_field('author'); ?></h2>
 
             <?php the_content(); ?>
