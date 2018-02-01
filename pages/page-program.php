f4a96039190193a461c95d88c348955c95c379bf
Jure Cuhalev <jure@cuhalev.com>
2018-02-01 22:13:49 +0100

    Program prototype

---
+++ pages/page-program.php
@@ -0,0 +1,172 @@
+<?php
+/*
+	Template name: Schedule - At a glance
+*/
+?>
+<?php get_header(); ?>
+
+<style>
+    td {
+        vertical-align: top;
+    }
+</style>
+
+<div class="container">
+    <div class="row">
+        <div class="column">
+            <table class="table text-center">
+                <thead>
+                <tr>
+                    <th>Saturday 21/4</th>
+                    <th>Sunday 22/4</th>
+                    <th>Monday 23/4</th>
+                    <th>Tuesday 24/4</th>
+                    <th>Wednesday 25/4</th>
+                    <th>Thursday 26/4</th>
+                    <th>Kingsday</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td>8:00 - 9:00<br/>
+                        Registration Desk Open
+                    </td>
+                    <td>8:00 - 9:00<br/>
+                        Registration Desk Open
+                    </td>
+                    <td>8:00 - 9:00<br/>
+                        Registration Desk Open
+                    </td>
+                    <td rowspan="12" style="background-color: #f67f12;"></td>
+                </tr>
+                <tr>
+                    <td rowspan="7">9:00 - 17:00<br/>
+                        <b>Go-Gn meeting</b></td>
+                    <td rowspan="7">9:00 - 17:00<br/>
+                        <b>Go-Gn meeting</b></td>
+                    <td rowspan="3">9:00 - 17:00</td>
+                    <td>9:00 - 10:15<br/>
+                        <b>Welcome<br/>
+                            Opening Minister of Education<br/>
+                            Keynote Vincent Zimmer</b></td>
+                    <td>
+                        9:00 - 10:00<br/>
+                        <br/>
+                        <b>Keynote Erin McKiernan<br/>
+                            UNESCO OER Policy Update</b></td>
+
+                    <td>9:00 - 10:00<br/>
+                        <br/>
+                        <b>Keynote<br/>
+                            Annemies Broekgaarden</b></td>
+                </tr>
+                <tr>
+                    <td>10:15 - 10:45<br/>
+                        Coffee/Tea Break
+                    </td>
+                    <td>10:15 - 10:45<br/>
+                        Coffee/Tea Break
+                    </td>
+                    <td>10:15 - 10:45<br/>
+                        Coffee/Tea Break
+                    </td>
+                </tr>
+                <tr>
+                    <td>10:45 - 12:30<br/>
+                        <b>parallel sessions A1 (4)</b>
+                    </td>
+                    <td>
+                        10:30 - 12:35<br/>
+                        <b>parallel sessions B1 (5)</b>
+                    </td>
+                    <td>
+                        10:30 - 12:10<br/>
+                        <b>parallel sessions C1 (4)</b>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        12.30 - 17.00
+                        Registration Desk Open<br/>
+                    </td>
+                    <td>12:30 -13:30<br/>
+                        Lunch
+                    </td>
+                    <td>12:35 -13:35<br/>
+                        Lunch<br/>
+                        <b>poster session</b></td>
+                    <td>12:10 -13:15<br/>
+                        Lunch
+                    </td>
+                </tr>
+                <tr>
+                    <td rowspan="3"><b>14.00 - 17.00<br />
+                            <b></b>Pre-Conference<br/>
+                            TU Delft Open & Online<br/>
+                            Education</b>
+                    </td>
+                    <td>13:30 - 14:15<br/>
+                        <b>OE Consortium Update by new Executive Director</b></td>
+                    <td>13:30 - 14:15<br/>
+                        <b>13:35 - 14:20</b>
+                        <b>Featured Speaker 1</b></td>
+                    <td>13:15 - 14:30<br/>
+                        <b>parallel sessions C2 (3)</b></td>
+                </tr>
+                <tr>
+                    <td>15:40 - 16:10<br/>
+                        Coffee/Tea Break
+                    </td>
+                    <td>
+                        15:45 - 16:15<br/>
+                        Coffee/Tea Break
+                    </td>
+                    <td>15:30  - 16:00<br/>
+                        Coffee/Tea Break
+                    </td>
+                </tr>
+                <tr>
+                    <td rowspan="2">
+                    16:10 - 17:30<br/>
+                        <b>parallel sessions A3 (3)</b>
+                    </td>
+                    <td rowspan="2">
+                        16:15 - 17:30<br />
+                        <b>parallel sessions B3 (3)</b>
+                    </td>
+                    <td rowspan="2"><b>Optional Madurodam Tour</b></td>
+                </tr>
+                <tr>
+                    <td rowspan="4"></td>
+                    <td rowspan="4"></td>
+                    <td rowspan="5"></td>
+                </tr>
+                <tr>
+                    <td><br /></td>
+                    <td><br /></td>
+                    <td><br /></td>
+                </tr>
+
+                <tr>
+                    <td rowspan="2">18:30 - 20:30<br/>
+                        <b>Opening Reception</b>
+                    </td>
+                    <td><br /></td>
+                    <td><br /></td>
+                </tr>
+                <tr>
+                    <td>19:00 - 23:00<br/>
+                        <b>Gala Diner</b>
+                    </td>
+                    <td></td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+
+<?php get_footer(); ?>
